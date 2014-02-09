e37ec6fff7c6ca71e4274480d114d8baa7ade815
BENJAMIN SCHENKER <schenk@senior12.ee.cooper.edu>
2014-02-08 20:48:07 -0500

    added new structure, started tutorial

--- hello.py
+++ hello.py
@@ -1,7 +1,25 @@
 import os
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+from app import views
+
+
+#SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","postgresql://qblbhphbktvdte:iYAAtsP_8ey0Y8o_rdCvNrS2b/dfephno04q3i00")
+
+# configuration
+DATABASE = 'ec2-50-16-201-126.compute-1.amazonaws.com'
+DEBUG = True
+SECRET_KEY = 'yowza'
+USERNAME = 'qblbhphbktvdte'
+PASSWORD = '-iYAAtsP_8ey0Y8o_rdCvNrS2b'
+
+app.config.from_object(__name__)
+db=SQLAlchemy(app)
+
+#def connect_db():
+#	return SQLAlchemy.connect(app.config['DataBase'])
 
 @app.route('/')
 def hello_world():
