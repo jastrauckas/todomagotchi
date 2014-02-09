09f963151a747b94f234908c9df0252ecd83f763
BENJAMIN SCHENKER <schenk@senior12.ee.cooper.edu>
2014-02-09 02:11:47 -0500

    added some restful features

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
-import pymongo, re
+from flask import Flask, render_template, request, jsonify, make_response, redirect, url_for
+import pymongo, re, json
+from bson import json_util
 from pymongo import MongoClient
 from app import app
 
@@ -7,6 +8,10 @@ client = MongoClient("mongodb://admin:123@troup.mongohq.com:10032/todo")
 
 db = client.todo
 user_collection = db.user_collection
+task_collection = db.task_collection
+def toJson(data):
+#Convert Mongo object(s) to JSON
+	return json.dumps(data, default=json_util.default)
 
 @app.route('/')
 @app.route('/login', methods=['POST', 'GET'])
@@ -20,7 +25,9 @@ def login():
 		 	userinfo = user_collection.find_one({'username' : username })
 		 	print userinfo
 		 	if userinfo!=None and userinfo['password']==password:
-				return render_template('user.html')
+				resp = make_response(render_template('index.html'))
+				resp.set_cookie('username',username)
+				return resp
 			else:
 			    return render_template("login.html", errmsg="Login Error")
         else:
@@ -38,21 +45,82 @@ def signup():
                 user = {	
                     'username' : username,
                     'password' : password
-                }
+                    }
                 user_collection.insert(user)
-                return render_template('user.html')
+                resp = make_response(render_template('index.html'))
+                resp.set_cookie('username',username)
+                return resp
 
             else:
                 return render_template('signup.html', error = 'Username already exists.')
 
         else:
             return render_template('signup.html', error = 'Both fields must be complete.')
+@app.route('/newtask',methods=['POST'])
+def newtask():
+	username = request.cookies.get('username')
+	header = request.form['header']
+	name = request.form['name']
+	value = request.form['value']
+	#created = request.form['created']
+	if username and header and name and value:
+		task = {	
+                    'username' : username,
+                    'header' : header,
+                    'name' : name,
+                    'value' : value,
+                    'complete' : False#,
+                    #'created' : created
+                }
+		task_collection.insert(task)
+		return redirect(url_for('refresh'))
+		
+@app.route('/refresh')
+def refresh():
+	cursor = task_collection.find()
+	results = []
+	for todo in cursor:
+		results.append(todo)
+	return toJson(results)
+	#return redirect(url_for('login'))
+"""
+	else:
+		username = request.cookies.get('username')
+		if username:
+			cursor = task_collection.find({'username' : username })
+			results = []
+			for todo in cursor:
+				results.append(todo)
+			return toJson(results)
+		else:
+			return redirect(url_for('login'))"""
+
+@app.route('/addtask', methods=['POST', 'GET'])
+def addtask():
+	if request.method=='GET':
+		return render_template("addtask.html")
+	elif request.method=='POST':
+		username = request.cookies.get('username')
+		header = request.form['header']
+		name = request.form['name']
+		value = request.form['value']
+		#created = request.form['created']
+		if username and header and name and value:
+			task = {	
+		                'username' : username,
+		                'header' : header,
+		                'name' : name,
+		                'value' : value,
+		                'complete' : False#,
+		                #'created' : created
+		            }
+			task_collection.insert(task)
+			return redirect(url_for('refresh'))
+		
+	
 @app.route('/index')
 def index():
-    user = { 'nickname': 'Miguel' } # fake user
-    return render_template("index.html",
-        title = 'Home',
-        user = user)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
