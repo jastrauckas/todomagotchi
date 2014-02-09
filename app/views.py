44839689aaf5d8362b38b11efe466b345f3eff33
BENJAMIN SCHENKER <schenk@senior12.ee.cooper.edu>
2014-02-09 08:14:33 -0500

    fixed heroku reqs (pymongo) again really hope this works now grrr

--- app/views.py
+++ app/views.py
@@ -124,7 +124,7 @@ def index():
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.debug = True
-    app.run(host='0.0.0.0', port=port)
-    print "App running on port 5000."
+    #port = int(os.environ.get('PORT', 5000))
+#    app.debug = True
+    app.run()#(host='0.0.0.0', port=port)
+    #print "App running on port 5000."
